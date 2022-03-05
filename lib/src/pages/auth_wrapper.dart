c75ed9e7374ab1e0cb3b106a8ee2bcaab0b045f7
Bavon101 <bavonfate@gmail.com>
2022-03-05 12:11:09 +0300

    Create auth wrapper to controll google auth,also navigate the user to home when loged or show firebase auth page

---
+++ lib/src/pages/auth_wrapper.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:shopping_app/src/pages/cart_view.dart';
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      // If the user is already signed-in, use it as initial data
+      initialData: FirebaseAuth.instance.currentUser,
+      builder: (context, snapshot) {
+        // User is not signed in
+        if (!snapshot.hasData) {
+          return const SignInScreen(providerConfigs: [
+            EmailProviderConfiguration(),
+          ]);
+        }
+
+        // Render application if authenticated
+        return const CartView();
+      },
+    );
+  }
+}
